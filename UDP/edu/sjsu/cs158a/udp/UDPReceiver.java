0fa11370d69b5688f9a9ade0d0f0756869bb8838
ben reed <ben.reed@sjsu.edu>
2023-02-08 14:56:20 -0800

    class changes

--- UDP/edu/sjsu/cs158a/udp/UDPReceiver.java
+++ UDP/edu/sjsu/cs158a/udp/UDPReceiver.java
@@ -3,16 +3,35 @@ package edu.sjsu.cs158a.udp;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
 
 public class UDPReceiver {
     public static void main(String[] arg) throws IOException {
-        var sock = new DatagramSocket(2323);
-        var bytes = new byte[512];
-        var packet = new DatagramPacket(bytes, bytes.length);
-        while (true) {
-            sock.receive(packet);
-            System.out.println("Got: " + new String(bytes));
+        try (var sock = new DatagramSocket(2323)) {
+            System.out.println(sock.getLocalSocketAddress());
+            System.out.println(sock.getRemoteSocketAddress());
+            sock.setSoTimeout(5000);
+            var bytes = new byte[512];
+            var bb = ByteBuffer.wrap(bytes);
+            var packet = new DatagramPacket(bytes, bytes.length);
+            while (true) {
+                try {
+                    sock.receive(packet);
+                } catch (SocketTimeoutException e) {
+                    System.out.println("i'm bored");
+                    System.exit(0);
+                }
+                var len = packet.getLength();
+                bb.position(0);
+                while (bb.position() < len) {
+                    System.out.println("bb pos " + bb.position());
+                    var rand = bb.getInt();
+                    System.out.println("bb pos " + bb.position());
+//                System.out.println(packet.getSocketAddress() + " Got: " + new String(bytes, 0, len));
+                    System.out.println(packet.getSocketAddress() + " Got: " + rand);
+                }
+            }
         }
     }
 }
--- UDP/edu/sjsu/cs158a/udp/UDPSender.java
+++ UDP/edu/sjsu/cs158a/udp/UDPSender.java
@@ -1,19 +1,31 @@
 package edu.sjsu.cs158a.udp;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class UDPSender {
     public static void main(String[] arg) throws IOException {
-        var sock = new DatagramSocket();
-        var bytes = "hello!".getBytes();
-        var packet = new DatagramPacket(bytes, bytes.length);
-        packet.setAddress(InetAddress.getByName("127.0.0.1"));
-        packet.setPort(2323);
-        sock.send(packet);
+        try (var sock = new DatagramSocket()) {
+            System.out.println(sock.getLocalSocketAddress());
+            System.out.println(sock.getRemoteSocketAddress());
+            var count = 3;
+            byte[] bytes = new byte[4*count];
+            var bb = ByteBuffer.wrap(bytes);
+            for (int i = 0; i < count; i++) {
+                var rand = new Random().nextInt();
+                bb.putInt(rand);
+            }
+            var packet = new DatagramPacket(bytes, bytes.length);
+            packet.setAddress(InetAddress.getByName("127.0.0.1"));
+            packet.setPort(2323);
+            sock.send(packet);
+        }
     }
 }
